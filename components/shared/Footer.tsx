0053aa829e7e8eaf2d403ba625f85c372bd777ae
asseph <davidkano.dk@gmail.com>
2024-06-22 15:30:39 +0700

    footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -4,7 +4,7 @@ import { TbWorldWww } from "react-icons/tb";
 
 function Footer() {
   return (
-    <footer className='w-full flex items-center justify-between gap-2 absolute bottom-0 left-0 z-50 p-4'>
+    <footer className='w-full flex items-center justify-between gap-2 relative lg:absolute bottom-0 left-0 z-50 p-4'>
       <span className='text-xs'>Powered by @ 2024 Parity Finance</span>
       <div className="flex items-center justify-center gap-2">
         <a href="https://twitter.com/parityfinance" target="_blank" rel="noreferrer" className='text-xs'>
