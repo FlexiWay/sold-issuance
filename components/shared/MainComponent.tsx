3e2e58911ed9a45a967f275a0171a82e663f6b4e
asseph <davidkano.dk@gmail.com>
2024-07-01 21:45:31 +0700

    add missing field

--- components/shared/MainComponent.tsx
+++ components/shared/MainComponent.tsx
@@ -39,29 +39,29 @@ export default function MainComponent() {
         {/* Devnet USDC faucet */}
         {connection.rpcEndpoint.includes("devnet") ? (
           <div className="w-full max-w-md bg-brand-bg rounded-lg shadow-md border border-[#E5E7EB0A] min-h-10 mt-8">
-          <form className="w-full flex flex-col items-center justify-start gap-6 p-6 py-8">
-            <div className="w-full flex flex-col items-start justify-start gap-2">
-              <label htmlFor="usdcAmount" className="text-xs">Amount of USDC to Mint</label>
-              <input
-                type="number"
-                id="usdcAmount"
-                className="w-full p-2 border rounded"
-                placeholder="Enter amount"
-                onChange={(e) => setDevnetFaucetAmount(parseInt(e.target.value))}
-                value={devnetFaucetAmount}
-              />
-            </div>
-            <div className="w-full flex items-center justify-center">
-              <button
-                className="w-full h-full rounded-lg text-white py-4 px-8 disabled:cursor-not-allowed uppercase bg-brand-first bg-opacity-100 hover:bg-opacity-20 ease-in-out transition-all duration-300"
-                onClick={handleMintDevnetUSDC}
-                disabled={devnetFaucetLoading}
-              >
-                {devnetFaucetLoading && <Spin size="small" />} {!devnetFaucetLoading && `Mint USDC`}
-              </button>
-            </div>
-          </form>
-        </div>
+            <form className="w-full flex flex-col items-center justify-start gap-6 p-6 py-8">
+              <div className="w-full flex flex-col items-start justify-start gap-2">
+                <label htmlFor="usdcAmount" className="text-xs">Amount of USDC to Mint</label>
+                <input
+                  type="number"
+                  id="usdcAmount"
+                  className="w-full p-2 border rounded"
+                  placeholder="Enter amount"
+                  onChange={(e) => setDevnetFaucetAmount(parseInt(e.target.value))}
+                  value={devnetFaucetAmount}
+                />
+              </div>
+              <div className="w-full flex items-center justify-center">
+                <button
+                  className="w-full h-full rounded-lg text-white py-4 px-8 disabled:cursor-not-allowed uppercase bg-brand-first bg-opacity-100 hover:bg-opacity-20 ease-in-out transition-all duration-300"
+                  onClick={handleMintDevnetUSDC}
+                  disabled={devnetFaucetLoading}
+                >
+                  {devnetFaucetLoading && <Spin size="small" />} {!devnetFaucetLoading && `Mint USDC`}
+                </button>
+              </div>
+            </form>
+          </div>
         ) : null}
 
         <div className="w-full max-w-md bg-brand-bg rounded-lg shadow-md border border-[#E5E7EB0A]  min-h-10">
@@ -343,17 +343,51 @@ export default function MainComponent() {
                 </div>
                 {/* to */}
                 <div className="w-full flex flex-col items-start justify-start gap-2">
-                  <div className="w-full flex items-center justify-between">
+                  <div className="w-full flex items-center justify-start">
                     <span className="text-xs">To receive</span>
-                    {/* <div className="flex items-center justify-end opacity-50 gap-1">
-                      <CiWallet className="w-3 h-3" />
-                      <span className="text-[10px]">
-                        {userBalanceUSDC.toLocaleString()}
-                      </span>
-                    </div> */}
+                    {/* <div className=" flex items-center justify-end opacity-50 gap-1">
+                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M0.166016 4.25H11.2493C11.4041 4.25 11.5524 4.31146 11.6618 4.42085C11.7712 4.53025 11.8327 4.67862 11.8327 4.83333V10.6667C11.8327 10.8214 11.7712 10.9697 11.6618 11.0791C11.5524 11.1885 11.4041 11.25 11.2493 11.25H0.749349C0.594639 11.25 0.446266 11.1885 0.33687 11.0791C0.227474 10.9697 0.166016 10.8214 0.166016 10.6667V4.25ZM0.749349 0.75H9.49935V3.08333H0.166016V1.33333C0.166016 1.17862 0.227474 1.03025 0.33687 0.920854C0.446266 0.811458 0.594639 0.75 0.749349 0.75ZM7.74935 7.16667V8.33333H9.49935V7.16667H7.74935Z" fill="#69707D" />
+                    </svg>
+                    <span className="text-[10px]">
+                      {userBalancePUSD.toLocaleString()}
+                    </span>
+                  </div> */}
                   </div>
                   <div className="relative w-full flex items-center justify-start">
-
+                    <Image
+                      width={40}
+                      height={40}
+                      src="/usdc.svg"
+                      alt="usdc"
+                      className="w-10 h-10 absolute z-30 left-3 top-1/2 -translate-y-1/2 rounded-full"
+                    />
+                    <input
+                      type="number"
+                      disabled
+                      className="w-full input text-end bg-[#1B1E24] px-16 pr-6 py-6 pb-10 relative !text-opacity-80 !text-transparent"
+                      placeholder=""
+                      value={amount}
+                    />
+                    <span className="pr-6 absolute right-0 z-0 top-1/2 -translate-y-[55%] text-[14px] opacity-100 flex flex-col items-end justify-center -gap-0 pointer-events-none">
+                      <span className="text-[20px] -mb-1 text-white">
+                        {amount}
+                      </span>
+                      <span className="text-xs opacity-50">${amount}</span>
+                    </span>
+                    <div className="absolute top-1/2 -translate-y-1/2 left-16 flex flex-col items-start justify-start gap-1">
+                      <span className="font-bold uppercase text-[18px] -mb-2">
+                        USDC
+                      </span>
+                      <div className="flex items-center justify-start gap-1">
+                        <div className="flex items-center justify-start gap-1">
+                          <span className="text-xs opacity-40">Balance:</span>
+                          <span className="text-xs ">
+                            {userBalanceUSDC.toLocaleString()}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
 
